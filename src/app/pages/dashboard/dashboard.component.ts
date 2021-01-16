1a18de5cb982fbda025bc8acb08923c1aac76226
vents04 <vencidim04@gmail.com>
2021-01-16 22:50:40 +0200

    Dashboard page without support

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -9,6 +9,99 @@ export class DashboardComponent implements OnInit {
 
   constructor() { }
 
+  urls : Array<any> = [
+    {
+      "url" : "http://localhost",
+      "title" : "test",
+      "favicon" : "",
+      "period": "5min",
+      "favorites": false,
+      "logs" : [
+        {
+          "status": 100,
+          "text": "Information",
+          "dateAndTime": "20-20-2020"
+        },
+        {
+          "status": 200,
+          "text": "Success",
+          "dateAndTime": "20-20-2020"
+        },
+        {
+          "status": 500,
+          "text": "Server error",
+          "dateAndTime": "20-20-2020"
+        },
+        {
+          "status": 100,
+          "text": "Information",
+          "dateAndTime": "20-20-2020"
+        },
+        {
+          "status": 100,
+          "text": "Information",
+          "dateAndTime": "20-20-2020"
+        }
+      ]
+    },
+    {
+      "url" : "http://localhost",
+      "title" : "test",
+      "favicon" : "",
+      "period": "5min",
+      "favorites": true,
+      "logs" : [
+        {
+          "status": 200,
+          "text": "Success",
+          "dateAndTime": "20-20-2020"
+        }
+      ]
+    },
+    {
+      "url" : "http://localhost",
+      "title" : "test",
+      "favicon" : "",
+      "period": "5min",
+      "favorites": false,
+      "logs" : [
+        {
+          "status": 300,
+          "text": "Redirection",
+          "dateAndTime": "20-20-2020"
+        }
+      ]
+    },
+    {
+      "url" : "http://localhost",
+      "title" : "test",
+      "favicon" : "",
+      "period": "5min",
+      "favorites": true,
+      "logs" : [
+        {
+          "status": 400,
+          "text": "Client error",
+          "dateAndTime": "20-20-2020"
+        }
+      ]
+    },
+    {
+      "url" : "http://localhost",
+      "title" : "test",
+      "favicon" : "",
+      "period": "5min",
+      "favorites": false,
+      "logs" : [
+        {
+          "status": 500,
+          "text": "Server error",
+          "dateAndTime": "20-20-2020"
+        }
+      ]
+    },
+  ];
+
   ngOnInit(): void {
   }
 
