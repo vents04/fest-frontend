1e509817db73611d43c6a2f0333d0fb30fc961f0
vents04 <vencidim04@gmail.com>
2021-01-17 14:01:02 +0200

    Some requests updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
+import { HttpResponse } from '@angular/common/http';
 import { Route } from '@angular/compiler/src/core';
 import { Component } from '@angular/core';
 import { ActivatedRoute, Event, Router } from '@angular/router';
-import { AuthService } from './service/auth.service';
+import { AuthService } from './services/auth-service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ import { AuthService } from './service/auth.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'fest-frontend';
   showNavbar: boolean = false;
   showDropdown: boolean = false;
@@ -21,14 +23,27 @@ export class AppComponent {
   email: string = "";
 
   ngOnInit(): void {
+    this.authService.checkToken().subscribe((res: HttpResponse<any>) => {
+      const responseBody = JSON.parse(res.body);
+      (res.status == 400) ? this.router.navigate(['/login']) : this.setProfileDetails(responseBody.username, responseBody.email);
+    })
     this.setDeviceType();
-    // get profile details and update username and email //
-    this.username = "username";
-    this.email = "email@email.com";
+  }
+
+  setProfileDetails(username : string, email : string) {
+    this.username = username;
+    this.email = email;
   }
 
   constructor(private authService: AuthService, private router: Router) {
-    this.showNavbar = this.authService.checkToken()
+    /*
+    this.authService.checkToken().subscribe((res: HttpResponse<any>) => {
+      if(res.status === 200){
+        this.showNavbar = true;
+      }
+    }); 
+    */
+   this.showNavbar = true;
   }
 
   openComponent(component: string) {
