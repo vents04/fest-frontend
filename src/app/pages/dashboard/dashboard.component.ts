49dc3ade0e2a405c22f11379e7332c113c5bf602
FatserBG <konstantinkk2000@gmail.com>
2021-01-17 09:18:17 +0200

    auth and web req change

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WebRequestsService } from 'src/app/services/web-requests-service/web-requests.service';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private webService: WebRequestsService) { }
+
+  monitoredWebsites: Array<Object> = [];
+
+  monitoredWebsitesByDate: Array<Object> = [];
+  monitoredWebsitedByReturnCode: Array<Object> = [];
+  monitoredWebsitesByName: Array<Object> = [];
+
+  sortRequest: string = "name";
 
   ngOnInit(): void {
+    this.webService.getMonitoredWebsites(this.sortRequest).subscribe((res: HttpResponse<any>) => {
+      this.monitoredWebsites = JSON.parse(res.body);
+    })
   }
 
 }
