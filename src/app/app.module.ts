735094036503eef885d58fae5d4efd0bd618ee7c
vents04 <vencidim04@gmail.com>
2021-01-17 11:20:49 +0200

    A lot of changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
-import { ProfileComponent } from './pages/profile/profile.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AddUrlComponent } from './pages/add-url/add-url.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
-    ProfileComponent,
-    DashboardComponent
+    DashboardComponent,
+    AddUrlComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -18,26 +18,34 @@ export class DashboardComponent implements OnInit {
       "favorites": false,
       "logs" : [
         {
+          "Id": "testId1",
           "status": 100,
           "text": "Information",
+          "timeToComplete": 19103031,
           "dateAndTime": "20-20-2020"
         },
         {
+          "Id": "testId2",
           "status": 200,
           "text": "Success",
+          "timeToComplete": 19103031,
           "dateAndTime": "20-20-2020"
         },
         {
+          "Id": "testId3",
           "status": 500,
           "text": "Server error",
+          "timeToComplete": 19103031,
           "dateAndTime": "20-20-2020"
         },
         {
+          "Id": "testId4",
           "status": 100,
           "text": "Information",
           "dateAndTime": "20-20-2020"
         },
         {
+          "Id": "testId5",
           "status": 100,
           "text": "Information",
           "dateAndTime": "20-20-2020"
@@ -98,7 +106,37 @@ export class DashboardComponent implements OnInit {
     },
   ];
 
+  openedLog: any;
+  showLogModal: boolean = false;
+
+  statusCodeTexts = new Map<number, string>();
+
   ngOnInit(): void {
+    this.statusCodeTexts.set(100, "Information");
+    this.statusCodeTexts.set(200, "Success");
+    this.statusCodeTexts.set(300, "Redirection");
+    this.statusCodeTexts.set(400, "Client error");
+    this.statusCodeTexts.set(500, "Server error");
+  }
+
+  openLogModal(id: string) {
+    this.showLogModal = true;
+    for(let url of this.urls) {
+      for(let log of url.logs) {
+        if(log.Id == id) {
+          log.url = url.url;
+          this.openedLog = log;
+        }
+      }
+    }
+
+    let logModal = document.querySelector("#log-modal") as HTMLElement;
+    logModal.classList.add("is-active");
+  }
+
+  closeLogModal() {
+    let logModal = document.querySelector("#log-modal") as HTMLElement;
+    logModal.classList.remove("is-active");
   }
 
 }
